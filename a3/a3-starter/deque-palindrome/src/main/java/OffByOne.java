0aea24440c663f1ce03179a89fe401bd67efdc8e
Green-Tea <35417429+Green-Tea@users.noreply.github.com>
2022-11-01 22:56:28 +0700

    Delete a3/a3-starter directory

--- a3/a3-starter/deque-palindrome/src/main/java/OffByOne.java
+++
@@ -1,7 +0,0 @@
-public class OffByOne implements CharacterComparator{
-
-    @Override
-    public boolean equalChars(char x, char y) {
-        return (int) x - (int) y == 1 || (int) x - (int) y == -1;
-    }
-}
